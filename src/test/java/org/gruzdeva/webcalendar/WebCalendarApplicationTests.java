de65c28b149bc4c9086d744d28efa8f4716a815a
Ann Gruzdeva <theghostlyspirit@outlook.com>
2025-01-14 16:36:36 +0300

    Initial commit

---
+++ src/test/java/org/gruzdeva/webcalendar/WebCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gruzdeva.webcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
